a953fffd923040742685fd6213ab67aa7e089ee5
Smattacus <Smattacus@gmail.com>
2023-08-14 21:33:59 -0500

    Use nest generate; tokens and cipher module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CipherModule } from './modules/cipher.module';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
-  imports: [],
+  imports: [CipherModule, TokensModule],
   controllers: [AppController],
   providers: [AppService],
 })
